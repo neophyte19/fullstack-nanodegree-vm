cd1423e43fe10c570b12d969f0f4e46881abe1cd
neophyte19 <neophyte19@users.noreply.github.com>
2017-06-01 22:03:21 -0700

    Update tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,12 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
+\c tournament;
 
+CREATE TABLE players(id serial primary key, name text);
+
+CREATE TABLE matches(id serial primary key, 
+                     p1 integer references players(ID), 
+                     p2 integer references players(ID), 
+                     win integer references players(ID));
